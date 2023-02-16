12ab9dbe4647dcbc76bd50f6bb5380fc1aecb6d3
Dmitry Kutsenko <d2emonium@gmail.com>
2023-02-16 11:12:38 +0300

    Fix eslint errors

--- front/src/services/fieldAPI/data/fieldTypes.js
+++ front/src/services/fieldAPI/data/fieldTypes.js
@@ -1,11 +1,11 @@
-import { ICON_BANK, ICON_CASINO, ICON_JAIL } from "./icons";
+import * as icons from './icons';
 
 const FIELD_TYPE_COMPANY = 1;
 const FIELD_TYPE_BANK = 2;
 const FIELD_TYPE_CASINO = 3;
 const FIELD_TYPE_JAIL = 4;
 
-export const newField = (fieldType, title, icon, teleport=null, data={}) => ({
+const field = (fieldType, title, icon, teleport = null, data = {}) => ({
   fieldType,
   icon,
   teleport,
@@ -13,17 +13,25 @@ export const newField = (fieldType, title, icon, teleport=null, data={}) => ({
   ...data,
 });
 
-export const newBank = (payment, teleport=null) => newField(
+export const bank = (payment, teleport = null) => field(
   FIELD_TYPE_BANK,
   'Банк',
-  ICON_BANK,
+  icons.ICON_BANK,
   teleport,
   {
     payment,
   },
 );
 
-export const newCompany = (title, cost, icon, teleport=null) => newField(
+export const casino = (teleport = null) => field(
+  FIELD_TYPE_CASINO,
+  'Рулѣтка',
+  icons.ICON_CASINO,
+  teleport,
+  {},
+);
+
+export const company = (title, cost, icon, teleport = null) => field(
   FIELD_TYPE_COMPANY,
   title,
   icon,
@@ -33,21 +41,12 @@ export const newCompany = (title, cost, icon, teleport=null) => newField(
   },
 );
 
-export const newCasino = (teleport=null) => newField(
-  FIELD_TYPE_CASINO,
-  'Рулѣтка',
-  ICON_CASINO,
-  teleport,
-  {},
-);
-
-export const newJail = (fine, teleport=null) => newField(
+export const jail = (fine, teleport = null) => field(
   FIELD_TYPE_JAIL,
   'Околотокъ',
-  ICON_JAIL,
+  icons.ICON_JAIL,
   teleport,
   {
     fine,
   },
 );
-    
--- front/src/services/fieldAPI/data/index.js
+++ front/src/services/fieldAPI/data/index.js
@@ -1,65 +1,73 @@
-import { enumerate, makeIndices } from "../../utils";
-import { newBank, newCasino, newCompany, newJail } from "./fieldTypes";
-import * as icons from "./icons";
+import {
+  enumerate,
+  makeIndices,
+} from '../../utils';
+import {
+  bank,
+  casino,
+  company,
+  jail,
+} from './fieldTypes';
+import * as icons from './icons';
 
 const FIELDS = makeIndices(enumerate([
   // 1-11
-  newBank(1000),
-  newCompany('Руссо-Балтъ', 9000, icons.ICON_AUTO),
-  newCompany('Ньюпортъ', 20000, icons.ICON_AVIATION),
-  newCompany('Доходный Домъ', 9000, icons.ICON_HOTEL, 20),
-  newCasino(),
-  newCompany('Гарфункѣль', 10000, icons.ICON_MUSIC),
-  newCompany('Елѣсѣѣвъ', 2000, icons.ICON_FOOD),
-  newCompany('Філіповъ', 1500, icons.ICON_FOOD),
-  newCompany('Табакъ', 1000, icons.ICON_SMOKING),
-  newCompany('Трактіръ', 5000, icons.ICON_FOOD),
-  newCompany('Квасъ', 7000, icons.ICON_FOOD),
+  bank(1000),
+  company('Руссо-Балтъ', 9000, icons.ICON_AUTO),
+  company('Ньюпортъ', 20000, icons.ICON_AVIATION),
+  company('Доходный Домъ', 9000, icons.ICON_HOTEL, 20),
+  casino(),
+  company('Гарфункѣль', 10000, icons.ICON_MUSIC),
+  company('Елѣсѣѣвъ', 2000, icons.ICON_FOOD),
+  company('Філіповъ', 1500, icons.ICON_FOOD),
+  company('Табакъ', 1000, icons.ICON_SMOKING),
+  company('Трактіръ', 5000, icons.ICON_FOOD),
+  company('Квасъ', 7000, icons.ICON_FOOD),
 
-  newJail(10000),
+  jail(10000),
 
   // 13-23
-  newCompany('Зінгѣръ', 3000, icons.ICON_FASHION),
-  newCompany('Сбітѣнь', 7000, icons.ICON_FOOD),
-  newCompany('Извозъ', 2000, icons.ICON_AUTO),
-  newCompany('Лѣпажъ', 12000, icons.ICON_GUN),
-  newCompany('Братія Райтъ', 20000, icons.ICON_AVIATION),
-  newBank(2000),
-  newCompany('Махорка', 5000, icons.ICON_SMOKING),
-  newCompany('Фордъ', 8000, icons.ICON_AUTO, 4),
-  newCompany('Голандъ і Голандъ', 2000, icons.ICON_GUN, 27),
-  newCasino(),
-  newCompany('Бобровъ і Ко', 2000, icons.ICON_SAIL),
+  company('Зінгѣръ', 3000, icons.ICON_FASHION),
+  company('Сбітѣнь', 7000, icons.ICON_FOOD),
+  company('Извозъ', 2000, icons.ICON_AUTO),
+  company('Лѣпажъ', 12000, icons.ICON_GUN),
+  company('Братія Райтъ', 20000, icons.ICON_AVIATION),
+  bank(2000),
+  company('Махорка', 5000, icons.ICON_SMOKING),
+  company('Фордъ', 8000, icons.ICON_AUTO, 4),
+  company('Голандъ і Голандъ', 2000, icons.ICON_GUN, 27),
+  casino(),
+  company('Бобровъ і Ко', 2000, icons.ICON_SAIL),
 
-  newJail(8000),
+  jail(8000),
 
   // 25-35
-  newCompany('Крупъ', 5000, icons.ICON_GUN),
-  newCompany('Савоі', 15000, icons.ICON_HOTEL, 45),
-  newCompany('Остінъ', 10000, icons.ICON_AUTO, 21),
-  newCompany('Сытінъ', 3000, icons.ICON_FOOD),
-  newBank(2000),
-  newCompany('Фіатъ', 20000, icons.ICON_AUTO),
-  newCasino(),
-  newCompany('Мѣха', 7000, icons.ICON_FASHION),
-  newCompany('Ціркъ', 7000, icons.ICON_MUSIC),
-  newCompany('Зауэръ', 2000, icons.ICON_GUN),
-  newCompany('Медъ', 7000, icons.ICON_FOOD),
+  company('Крупъ', 5000, icons.ICON_GUN),
+  company('Савоі', 15000, icons.ICON_HOTEL, 45),
+  company('Остінъ', 10000, icons.ICON_AUTO, 21),
+  company('Сытінъ', 3000, icons.ICON_FOOD),
+  bank(2000),
+  company('Фіатъ', 20000, icons.ICON_AUTO),
+  casino(),
+  company('Мѣха', 7000, icons.ICON_FASHION),
+  company('Ціркъ', 7000, icons.ICON_MUSIC),
+  company('Зауэръ', 2000, icons.ICON_GUN),
+  company('Медъ', 7000, icons.ICON_FOOD),
 
-  newJail(9000),
+  jail(9000),
 
   // 37-46
-  newCompany('Асторія', 2000, icons.ICON_HOTEL),
-  newBank(2000),
-  newCompany('Яхта', 3000, icons.ICON_SAIL),
-  newCompany('Кофій', 4000, icons.ICON_FOOD),
-  newCompany('Дагѣръ', 9000, icons.ICON_MUSIC),
-  newCompany('Салонъ', 8000, icons.ICON_MUSIC),
-  newCompany('Кольтъ', 3000, icons.ICON_GUN),
-  newCompany('Данхілъ', 5000, icons.ICON_SMOKING),
-  newCompany('Бѣнтли', 10000, icons.ICON_AUTO),
-  newCompany('Чай', 7000, icons.ICON_FOOD, 26),
-  newCompany('Тула', 4000, icons.ICON_GUN),
+  company('Асторія', 2000, icons.ICON_HOTEL),
+  bank(2000),
+  company('Яхта', 3000, icons.ICON_SAIL),
+  company('Кофій', 4000, icons.ICON_FOOD),
+  company('Дагѣръ', 9000, icons.ICON_MUSIC),
+  company('Салонъ', 8000, icons.ICON_MUSIC),
+  company('Кольтъ', 3000, icons.ICON_GUN),
+  company('Данхілъ', 5000, icons.ICON_SMOKING),
+  company('Бѣнтли', 10000, icons.ICON_AUTO),
+  company('Чай', 7000, icons.ICON_FOOD, 26),
+  company('Тула', 4000, icons.ICON_GUN),
 ]));
 
 export default FIELDS;
